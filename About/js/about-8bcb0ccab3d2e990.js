7fc3275054c4bc7ef2920b371f4bb741bbd89471
sachinray2021 <sachinraywork@gmail.com>
2023-10-15 09:45:45 +0530

    Update Name of College

--- About/js/about-8bcb0ccab3d2e990.js
+++ About/js/about-8bcb0ccab3d2e990.js
@@ -1,1 +1,164 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{5401:function(e,t,a){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return a(1746)}])},1746:function(e,t,a){"use strict";a.r(t);var i=a(5893),n=a(7294),s=a(5675),r=a.n(s),o=a(2711),c=a.n(o);a(1496);t.default=()=>((0,n.useEffect)((()=>{c().init({duration:500})}),[]),(0,i.jsx)("section",{id:"about",className:"py-10 text-gray-600 flex gap-20 items-center justify-center","data-aos":"fade-right",children:(0,i.jsxs)("div",{className:"text-center mt-14",children:[(0,i.jsx)("p",{className:"text-[#de0650] my-3 text-2xl mt-20 font-bold font-playFair",children:"Manav Rachna International Institute of Research and Studies (MRIIRS)"}),(0,i.jsxs)("div",{className:"flex md:flex-row flex-col-reverse item-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto",children:[(0,i.jsx)("div",{children:(0,i.jsx)("div",{className:"text-gray-600 my-3 mt-8",children:(0,i.jsx)("p",{className:"text-justify leading-8 font-sans text-lg text-gray-600",children:"Manav Rachna International Institute of Research and Studies (MRIIRS), formerly MRIU, a Deemed-to-be-University under section 3 of the UGC Act, 1956, and a NAAC accredited ‘A++’ Grade Institution, is a continuum of excellence from the Career Institute of Technology and Management (CITM). MRIIRS has been awarded a 5-Star QS rating for Teaching, Facilities & Social Responsibility. MRIIRS is ranked among top institutions by the Ministry of Education’s NIRF Ranking 2022. It strives for high-quality education in the fields of Engineering & Technology, Health & Applied Sciences, Management, Computer Applications, Hotel Management, Commerce, Humanities, Media, Architecture, Design, Liberal Arts, Economics, Applied Psychology and many more. MRIIRS is a Founder Member of the prestigious “College Board’s Indian Global Higher Education Alliance”."})})}),(0,i.jsx)("div",{className:"flex-1 flex justify-center items-center",children:(0,i.jsx)("div",{className:"lg:w-96 h-full relative sm:w-10/12 w-12/12 max-w-full mt-20",children:(0,i.jsx)(r(),{src:"/images/about/mr.png",width:500,height:500,className:"animate-translateAnimate"})})})]}),(0,i.jsx)("p",{className:"text-[#de0650] my-3 text-2xl mt-14 font-bold font-playFair","data-aos":"fade-right",children:"Department of Computer Science and Engineering (CSE)"}),(0,i.jsxs)("div",{className:"flex md:flex-row flex-col-reverse item-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto","data-aos":"fade-right",children:[(0,i.jsx)("div",{children:(0,i.jsx)("div",{className:"text-gray-600 my-3 mt-8",children:(0,i.jsx)("p",{className:"text-justify leading-8 font-sans text-lg text-gray-600",children:"The Department of Computer Science & Engineering (CSE) was established in 1997 with a focus on imparting the fundamental concepts both theoretically and practically. Keeping in view the growth in industry and increasing demand for computer professionals, the department also offers some industry associated courses. To meet the needs of industry and R&D organizations, the course curriculum has been carefully customised to meet the rising global outlook and focuses on forthcoming innovations in the field of Computer Science and Engineering. Computer Science and Engineering (CSE) is a highly sought-after course because it acts as a catalyst and allows one to keep up with the digital era. The platforms are incredibly diversified, with a huge range of job opportunities. This course is offered to students at the undergraduate as well as the postgraduate levels and students obtain the B.Tech and M.Tech degrees, respectively.The B.Tech (CSE) programme offered by the Department has been accredited thrice by the National Board of Accreditation (NBA) in 2003, 2007, 2018 and 2023."})})}),(0,i.jsx)("div",{className:"flex-1 flex justify-center items-center",children:(0,i.jsx)("div",{className:"lg:w-96 h-full relative sm:w-10/12 w-12/12 max-w-full mt-20",children:(0,i.jsx)(r(),{src:"/images/about/department.png",width:500,height:500,className:"animate-translateAnimate"})})})]}),(0,i.jsxs)("p",{className:"text-[#de0650] my-3 text-2xl mt-14 font-bold font-playFair","data-aos":"fade-right",children:["ICAICCIT - 2023 (International Conference on Advances in Computation,",(0,i.jsx)("br",{})," Communication and Information Technology)"]}),(0,i.jsxs)("div",{className:"flex md:flex-row flex-col-reverse item-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto","data-aos":"fade-right",children:[(0,i.jsx)("div",{children:(0,i.jsx)("div",{className:"text-gray-600 my-3 mt-8",children:(0,i.jsxs)("p",{className:"text-justify leading-8 font-sans text-lg text-gray-600",children:["The main objective of ",(0,i.jsx)("b",{children:"ICAICCIT-2023"})," is to invite all the Academicians, Researchers, and industrialists worldwide to participate and discuss the latest achievements in the advanced field of computing like Advancement in the Artificial Intelligence, Blockchain, Cyber Security, Distributed Computing and Internet of Things. The main goal of the conference is to provide a forum for high-quality reports on the state-of-the-art Theory, Technology, and Applications of advanced fields of computing as used in measurement, automation, and scientific research, in industry and business. The initiative provides a focused and highly interactive forum where both researchers and technologists have the opportunity to present and discuss leading research topics, developments, and future directions in their areas of interest. The importance of the conference is its vision to establish scientific contacts between research teams and scientists from different countries for future joint research collaborations."]})})}),(0,i.jsx)("div",{className:"flex-1 flex justify-center items-center",children:(0,i.jsx)("div",{className:"lg:w-96 h-full relative sm:w-10/12 w-12/12 max-w-full mt-20",children:(0,i.jsx)(r(),{src:"/images/about/aiccit.png",width:500,height:500,className:"animate-translateAnimate"})})})]})]})}))}},function(e){e.O(0,[774,888,179],(function(){return e(e.s=5401)})),_N_E=e.O()}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [521],
+  {
+    5401: function (e, t, a) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/about",
+        function () {
+          return a(1746);
+        },
+      ]);
+    },
+    1746: function (e, t, a) {
+      "use strict";
+      a.r(t);
+      var i = a(5893),
+        n = a(7294),
+        s = a(5675),
+        r = a.n(s),
+        o = a(2711),
+        c = a.n(o);
+      a(1496);
+      t.default = () => (
+        (0, n.useEffect)(() => {
+          c().init({ duration: 500 });
+        }, []),
+        (0, i.jsx)("section", {
+          id: "about",
+          className:
+            "py-10 text-gray-600 flex gap-20 items-center justify-center",
+          "data-aos": "fade-right",
+          children: (0, i.jsxs)("div", {
+            className: "text-center mt-14",
+            children: [
+              (0, i.jsx)("p", {
+                className:
+                  "text-[#de0650] my-3 text-2xl mt-20 font-bold font-playFair",
+                children:
+                  "Dr. Akhilesh Das Gupta Institute of Technology and Management (ADGITM)",
+              }),
+              (0, i.jsxs)("div", {
+                className:
+                  "flex md:flex-row flex-col-reverse item-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto",
+                children: [
+                  (0, i.jsx)("div", {
+                    children: (0, i.jsx)("div", {
+                      className: "text-gray-600 my-3 mt-8",
+                      children: (0, i.jsx)("p", {
+                        className:
+                          "text-justify leading-8 font-sans text-lg text-gray-600",
+                        children:
+                          "Manav Rachna International Institute of Research and Studies (MRIIRS), formerly MRIU, a Deemed-to-be-University under section 3 of the UGC Act, 1956, and a NAAC accredited ‘A++’ Grade Institution, is a continuum of excellence from the Career Institute of Technology and Management (CITM). MRIIRS has been awarded a 5-Star QS rating for Teaching, Facilities & Social Responsibility. MRIIRS is ranked among top institutions by the Ministry of Education’s NIRF Ranking 2022. It strives for high-quality education in the fields of Engineering & Technology, Health & Applied Sciences, Management, Computer Applications, Hotel Management, Commerce, Humanities, Media, Architecture, Design, Liberal Arts, Economics, Applied Psychology and many more. MRIIRS is a Founder Member of the prestigious “College Board’s Indian Global Higher Education Alliance”.",
+                      }),
+                    }),
+                  }),
+                  (0, i.jsx)("div", {
+                    className: "flex-1 flex justify-center items-center",
+                    children: (0, i.jsx)("div", {
+                      className:
+                        "lg:w-96 h-full relative sm:w-10/12 w-12/12 max-w-full mt-20",
+                      children: (0, i.jsx)(r(), {
+                        src: "/images/about/mr.png",
+                        width: 500,
+                        height: 500,
+                        className: "animate-translateAnimate",
+                      }),
+                    }),
+                  }),
+                ],
+              }),
+              (0, i.jsx)("p", {
+                className:
+                  "text-[#de0650] my-3 text-2xl mt-14 font-bold font-playFair",
+                "data-aos": "fade-right",
+                children:
+                  "Department of Computer Science and Engineering (CSE)",
+              }),
+              (0, i.jsxs)("div", {
+                className:
+                  "flex md:flex-row flex-col-reverse item-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto",
+                "data-aos": "fade-right",
+                children: [
+                  (0, i.jsx)("div", {
+                    children: (0, i.jsx)("div", {
+                      className: "text-gray-600 my-3 mt-8",
+                      children: (0, i.jsx)("p", {
+                        className:
+                          "text-justify leading-8 font-sans text-lg text-gray-600",
+                        children:
+                          "The Department of Computer Science & Engineering (CSE) was established in 1997 with a focus on imparting the fundamental concepts both theoretically and practically. Keeping in view the growth in industry and increasing demand for computer professionals, the department also offers some industry associated courses. To meet the needs of industry and R&D organizations, the course curriculum has been carefully customised to meet the rising global outlook and focuses on forthcoming innovations in the field of Computer Science and Engineering. Computer Science and Engineering (CSE) is a highly sought-after course because it acts as a catalyst and allows one to keep up with the digital era. The platforms are incredibly diversified, with a huge range of job opportunities. This course is offered to students at the undergraduate as well as the postgraduate levels and students obtain the B.Tech and M.Tech degrees, respectively.The B.Tech (CSE) programme offered by the Department has been accredited thrice by the National Board of Accreditation (NBA) in 2003, 2007, 2018 and 2023.",
+                      }),
+                    }),
+                  }),
+                  (0, i.jsx)("div", {
+                    className: "flex-1 flex justify-center items-center",
+                    children: (0, i.jsx)("div", {
+                      className:
+                        "lg:w-96 h-full relative sm:w-10/12 w-12/12 max-w-full mt-20",
+                      children: (0, i.jsx)(r(), {
+                        src: "/images/about/department.png",
+                        width: 500,
+                        height: 500,
+                        className: "animate-translateAnimate",
+                      }),
+                    }),
+                  }),
+                ],
+              }),
+              (0, i.jsxs)("p", {
+                className:
+                  "text-[#de0650] my-3 text-2xl mt-14 font-bold font-playFair",
+                "data-aos": "fade-right",
+                children: [
+                  "ICAICCIT - 2023 (International Conference on Advances in Computation,",
+                  (0, i.jsx)("br", {}),
+                  " Communication and Information Technology)",
+                ],
+              }),
+              (0, i.jsxs)("div", {
+                className:
+                  "flex md:flex-row flex-col-reverse item-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto",
+                "data-aos": "fade-right",
+                children: [
+                  (0, i.jsx)("div", {
+                    children: (0, i.jsx)("div", {
+                      className: "text-gray-600 my-3 mt-8",
+                      children: (0, i.jsxs)("p", {
+                        className:
+                          "text-justify leading-8 font-sans text-lg text-gray-600",
+                        children: [
+                          "The main objective of ",
+                          (0, i.jsx)("b", { children: "ICAICCIT-2023" }),
+                          " is to invite all the Academicians, Researchers, and industrialists worldwide to participate and discuss the latest achievements in the advanced field of computing like Advancement in the Artificial Intelligence, Blockchain, Cyber Security, Distributed Computing and Internet of Things. The main goal of the conference is to provide a forum for high-quality reports on the state-of-the-art Theory, Technology, and Applications of advanced fields of computing as used in measurement, automation, and scientific research, in industry and business. The initiative provides a focused and highly interactive forum where both researchers and technologists have the opportunity to present and discuss leading research topics, developments, and future directions in their areas of interest. The importance of the conference is its vision to establish scientific contacts between research teams and scientists from different countries for future joint research collaborations.",
+                        ],
+                      }),
+                    }),
+                  }),
+                  (0, i.jsx)("div", {
+                    className: "flex-1 flex justify-center items-center",
+                    children: (0, i.jsx)("div", {
+                      className:
+                        "lg:w-96 h-full relative sm:w-10/12 w-12/12 max-w-full mt-20",
+                      children: (0, i.jsx)(r(), {
+                        src: "/images/about/aiccit.png",
+                        width: 500,
+                        height: 500,
+                        className: "animate-translateAnimate",
+                      }),
+                    }),
+                  }),
+                ],
+              }),
+            ],
+          }),
+        })
+      );
+    },
+  },
+  function (e) {
+    e.O(0, [774, 888, 179], function () {
+      return e((e.s = 5401));
+    }),
+      (_N_E = e.O());
+  },
+]);
