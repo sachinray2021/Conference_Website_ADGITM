5ac5b7faf8436e84d8aa9bfee949e9222c3a4935
batra016 <yuktibatra016@gmail.com>
2023-10-15 17:55:58 +0530

    modify

--- important_dates/js/important_dates-838fc4d99950536b.js
+++ important_dates/js/important_dates-838fc4d99950536b.js
@@ -1,1 +1,125 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[49],{51:function(e,s,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/important_dates",function(){return t(7191)}])},7191:function(e,s,t){"use strict";t.r(s);var n=t(5893);t(7294);s.default=()=>(0,n.jsxs)("section",{className:"py-24 text-gray","data-aos":"fade-right",children:[(0,n.jsx)("div",{className:"text-center mt-14",children:(0,n.jsxs)("h3",{className:"text-gray-600 text-5xl font-bold font-playFair ",children:["Important ",(0,n.jsx)("span",{children:"Dates"})]})}),(0,n.jsx)("div",{className:"mt-10 justify-center  flex text-gray-600",children:(0,n.jsxs)("table",{className:"text-gray-600",children:[(0,n.jsxs)("tr",{className:"text-2xl",children:[(0,n.jsx)("td",{children:"Paper Submission Deadline:"}),(0,n.jsxs)("td",{children:["31",(0,n.jsx)("sup",{children:"st"})," August, 2023"]})]}),(0,n.jsxs)("tr",{className:"text-2xl",children:[(0,n.jsx)("td",{children:"Acceptance Notification:"}),(0,n.jsxs)("td",{children:["5",(0,n.jsx)("sup",{children:"th"})," September, 2023"]})]}),(0,n.jsxs)("tr",{className:"text-2xl",children:[(0,n.jsx)("td",{children:"Camera Ready Paper Submission Deadline:"}),(0,n.jsxs)("td",{children:["30",(0,n.jsx)("sup",{children:"th"})," September, 2023"]})]}),(0,n.jsxs)("tr",{className:"text-2xl",children:[(0,n.jsx)("td",{children:"Last Date for Registration:"}),(0,n.jsxs)("td",{children:["10",(0,n.jsx)("sup",{children:"th"})," October, 2023"]})]}),(0,n.jsxs)("tr",{className:"text-2xl",children:[(0,n.jsx)("td",{children:"Conference Dates:"}),(0,n.jsxs)("td",{children:["23",(0,n.jsx)("sup",{children:"rd"})," & 24",(0,n.jsx)("sup",{children:"th"})," November, 2023"]})]})]})})]})}},function(e){e.O(0,[774,888,179],(function(){return e(e.s=51)})),_N_E=e.O()}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [49],
+  {
+    51: function (e, s, t) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/important_dates",
+        function () {
+          return t(7191);
+        },
+      ]);
+    },
+    7191: function (e, s, t) {
+      "use strict";
+      t.r(s);
+      var n = t(5893);
+      t(7294);
+      s.default = () =>
+        (0, n.jsxs)("section", {
+          className: "py-24 text-gray",
+          "data-aos": "fade-right",
+          children: [
+            (0, n.jsx)("div", {
+              className: "text-center mt-14",
+              children: (0, n.jsxs)("h3", {
+                className: "text-gray-600 text-5xl font-bold font-playFair ",
+                children: [
+                  "Important ",
+                  (0, n.jsx)("span", { children: "Dates" }),
+                ],
+              }),
+            }),
+            (0, n.jsx)("div", {
+              className: "mt-10 justify-center  flex text-gray-600",
+              children: (0, n.jsxs)("table", {
+                className: "text-gray-600",
+                children: [
+                  (0, n.jsxs)("tr", {
+                    className: "text-2xl",
+                    children: [
+                      (0, n.jsx)("td", {
+                        children: "Paper Submission Deadline:",
+                      }),
+                      (0, n.jsxs)("td", {
+                        children: [
+                          "6",
+                          (0, n.jsx)("sup", { children: "th" }),
+                          " February, 2024",
+                        ],
+                      }),
+                    ],
+                  }),
+                  (0, n.jsxs)("tr", {
+                    className: "text-2xl",
+                    children: [
+                      (0, n.jsx)("td", {
+                        children: "Acceptance Notification:",
+                      }),
+                      (0, n.jsxs)("td", {
+                        children: [
+                          "15",
+                          (0, n.jsx)("sup", { children: "th" }),
+                          " March, 2024",
+                        ],
+                      }),
+                    ],
+                  }),
+                  (0, n.jsxs)("tr", {
+                    className: "text-2xl",
+                    children: [
+                      (0, n.jsx)("td", {
+                        children: "Camera Ready Paper Submission Deadline:",
+                      }),
+                      (0, n.jsxs)("td", {
+                        children: [
+                          "15",
+                          (0, n.jsx)("sup", { children: "th" }),
+                          " May, 2024",
+                        ],
+                      }),
+                    ],
+                  }),
+                  (0, n.jsxs)("tr", {
+                    className: "text-2xl",
+                    children: [
+                      (0, n.jsx)("td", {
+                        children: "Last Date for Registration:",
+                      }),
+                      (0, n.jsxs)("td", {
+                        children: [
+                          "30",
+                          (0, n.jsx)("sup", { children: "th" }),
+                          " May, 2024",
+                        ],
+                      }),
+                    ],
+                  }),
+                  (0, n.jsxs)("tr", {
+                    className: "text-2xl",
+                    children: [
+                      (0, n.jsx)("td", { children: "Conference Dates:" }),
+                      (0, n.jsxs)("td", {
+                        children: [
+                          "23",
+                          (0, n.jsx)("sup", { children: "rd" }),
+                          " & 24",
+                          (0, n.jsx)("sup", { children: "th" }),
+                          " July, 2024",
+                        ],
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            }),
+          ],
+        });
+    },
+  },
+  function (e) {
+    e.O(0, [774, 888, 179], function () {
+      return e((e.s = 51));
+    }),
+      (_N_E = e.O());
+  },
+]);
